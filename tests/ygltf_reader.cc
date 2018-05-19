d4192f108d3879c3d562eb5894ec47f8dca0cce1
Simon C Blyth <simon.c.blyth@gmail.com>
2018-05-19 15:37:58 +0800

    oyoctogl- update to BCM exported target approach

--- tests/ygltf_reader.cc
+++ tests/ygltf_reader.cc
@@ -121,6 +121,13 @@ void test_walk(const ygltf::glTF_t* gltf, int scene_idx=-1)
 
 int main(int argc, char** argv)
 {
+    if(argc < 2)
+    {
+        std::cout << argv[0] << std::endl ; 
+        std::cout << "Usage: expecting path to .gltf file " << std::endl ; 
+        return 0 ; 
+    }
+
     const std::string filename = argv[1] ; 
     std::cout << "load_gltf " << filename << std::endl ; 
 
